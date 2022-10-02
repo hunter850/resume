2cae4925868026ca470b9be12069dc86aed671bc
hunterluo <hunter850711@gmail.com>
2022-10-02 15:22:06 +0800

    create cover animation

--- src/pages/Home/components/CoverAnimation/index.jsx
+++ src/pages/Home/components/CoverAnimation/index.jsx
@@ -1,37 +1,67 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "./scss/coverAnimation.module.scss";
 import useClass from "@/hooks/useClass";
 import { useScrollbar } from "@/contexts/ScrollbarProvider";
+import useTimeout from "@/hooks/useTimeout";
 
-function CoverAnimation(props) {
-    const { showCover, setShowCover } = props;
+function CoverAnimation() {
     const {
-        cover__bg,
-        "cover__bg--off": cover__bg_off,
-        cover__texts,
-        "cover__enter-button": cover__enterButton,
+        desk,
+        book,
+        page,
+        flip,
+        cover,
+        "desk--disapear": desk_disapear,
+        "scale-animation": scale_animation,
+        "book__enter-button": book__enterButton,
     } = styles;
     const c = useClass();
+    const [enterAnimation, setEnterAnimation] = useState(false);
+    const [canClickThrough, setCanClickThrough] = useState(false);
     const { showScrollbar, hideScrollbar } = useScrollbar();
+    const [timer] = useTimeout();
+
+    function scaleHandler() {
+        setEnterAnimation(true);
+        timer(() => {
+            setCanClickThrough(true);
+        }, 1000);
+    }
+
     useEffect(() => {
-        if (showCover) {
-            hideScrollbar();
-        } else {
+        if (canClickThrough) {
             showScrollbar();
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [showCover]);
+    }, [canClickThrough]);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        timer(() => {
+            hideScrollbar();
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <>
-            <div className={c(cover__bg, { [cover__bg_off]: !showCover })}>
-                <ul className={cover__texts}>
-                    <li>Hi I'm Kevin</li>
-                    <li>
-                        <button className={cover__enterButton} onClick={() => setShowCover(false)}>
-                            enter
-                        </button>
-                    </li>
-                </ul>
+            <div className={c(desk, { [desk_disapear]: canClickThrough })}>
+                <div className={c(book, { [scale_animation]: enterAnimation })}>
+                    <div className={c(page, flip)}></div>
+                    <div className={cover}></div>
+                    <div className={page}>
+                        <ul>
+                            <li>不管你從哪裡獲得本書</li>
+                            <li>這裡記錄了 Kevin 的經歷</li>
+                            <li>在此請讓我向您介紹</li>
+                            <li>一位優質的前端工程師</li>
+                            <li>
+                                <button className={book__enterButton} onClick={scaleHandler}>
+                                    Enter
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={c(cover, flip)}></div>
+                </div>
             </div>
         </>
     );
