37cec5cf62a47ebc6ee43499e6dd18015441ce87
hunterluo <hunter850711@gmail.com>
2022-10-02 15:18:16 +0800

    remove useless hooks

--- src/hooks/useNextTick.jsx
+++
@@ -1,9 +0,0 @@
-import { useCallback } from "react";
-
-const useNextTick = () => {
-    return useCallback((fn) => {
-        return Promise.resolve().then(fn);
-    }, []);
-};
-
-export default useNextTick;
